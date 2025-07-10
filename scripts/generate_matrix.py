26477642d59f2d0f7ebd0753ed4fa2a4fa4ccd99
Weathercold <weathercold.scr@proton.me>
2025-07-10 11:13:47 -0400

    feat: init layout

--- scripts/generate_matrix.py
+++ scripts/generate_matrix.py
@@ -16,7 +16,7 @@ format_shields = {
 
 groups = []
 for keymap in keymaps:
-    for fmt in ["bt", "dongle"]:
+    for fmt in ["bt"]: # "dongle"
         groups.append({
             "keymap": keymap,
             "format": fmt,
